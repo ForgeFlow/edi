17dd83b2c33fe739307739c5e2ff93c8b00b0ae6
Lois Rilo <lois.rilo@forgeflow.com>
2021-09-06 10:31:57 +0200

    fixup! fixup! [TEMP] disabling constraint

--- edi/models/edi_exchange_record.py
+++ edi/models/edi_exchange_record.py
@@ -168,7 +168,7 @@ class EDIExchangeRecord(models.Model):
     ):
         """Handy method to no have to convert b64 back and forth."""
         self.ensure_one()
-        _logger.info("output_string type: %s" % type(output_string))
+        _logger.info("LOIS TEST - output_string type: %s" % type(output_string))
         if not isinstance(output_string, bytes):
             output_string = bytes(output_string, encoding)
         self[field_name] = base64.b64encode(output_string)
--- edi_storage/components/receive.py
+++ edi_storage/components/receive.py
@@ -1,7 +1,11 @@
 # Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+import logging
+
 from odoo.addons.component.core import Component
+
+_logger = logging.getLogger(__name__)
 
 
 class EDIStorageReceiveComponent(Component):
@@ -23,5 +27,7 @@ class EDIStorageReceiveComponent(Component):
         direction = self.exchange_record.direction
         filename = self.exchange_record.exchange_filename
         path = self._remote_file_path(direction, "pending", filename)
+        _logger.info("LOIS TEST - path: %s" % path)
         filedata = self.storage.get(path.as_posix())
+        _logger.info("LOIS TEST - filedata: %s" % filedata)
         return filedata
