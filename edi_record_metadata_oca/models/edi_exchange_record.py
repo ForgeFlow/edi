camptocamp/edi-framework

Add edi_record_metadata_oca

edi_record_metadata_oca/models/edi_exchange_record.py
@@ -1,0 +1,34 @@
+# Copyright 2023 Camptocamp SA
+# @author Simone Orsi <simahawk@gmail.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import json
+
+from odoo import api, fields, models
+
+from odoo.addons.base_sparse_field.models.fields import Serialized
+
+
+class EDIExchangeRecord(models.Model):
+
+    _inherit = "edi.exchange.record"
+
+    metadata = Serialized(
+        help="JSON-like metadata used for technical purposes.",
+        default={},
+    )
+    metadata_display = fields.Text(
+        compute="_compute_metadata_display",
+        help="Enable debug mode to be able to inspect data.",
+    )
+
+    @api.depends("metadata")
+    def _compute_metadata_display(self):
+        for rec in self:
+            rec.metadata_display = json.dumps(rec.data, sort_keys=True, indent=4)
+
+    def set_metadata(self, data):
+        self.metadata = data
+
+    def get_metadata(self):
+        return self.metadata

ForgeFlow/edi

edi_record_metadata_oca/models/edi_exchange_record.py
@@ -1,0 +1,34 @@
+# Copyright 2023 Camptocamp SA
+# @author Simone Orsi <simahawk@gmail.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import json
+
+from odoo import api, fields, models
+
+from odoo.addons.base_sparse_field.models.fields import Serialized
+
+
+class EDIExchangeRecord(models.Model):
+
+    _inherit = "edi.exchange.record"
+
+    metadata = Serialized(
+        help="JSON-like metadata used for technical purposes.",
+        default={},
+    )
+    metadata_display = fields.Text(
+        compute="_compute_metadata_display",
+        help="Enable debug mode to be able to inspect data.",
+    )
+
+    @api.depends("metadata")
+    def _compute_metadata_display(self):
+        for rec in self:
+            rec.metadata_display = json.dumps(rec.metadata, sort_keys=True, indent=4)
+
+    def set_metadata(self, data):
+        self.metadata = data
+
+    def get_metadata(self):
+        return self.metadata